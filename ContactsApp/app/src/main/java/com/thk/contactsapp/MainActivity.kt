55733a524a5095d1acb3aca47dbd924c8d229b77
Kang Taehee <dev.thk28@gmail.com>
2022-02-15 16:34:19 +0900

    add: toolbar 추가

--- ContactsApp/app/src/main/java/com/thk/contactsapp/MainActivity.kt
+++ ContactsApp/app/src/main/java/com/thk/contactsapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.thk.contactsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.thk.contactsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
     }
 }
