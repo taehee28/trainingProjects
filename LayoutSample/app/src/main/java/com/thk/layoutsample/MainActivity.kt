45cba114c815d93749da7e7adadc4090bbc5d489
Kang Taehee <dev.thk28@gmail.com>
2022-02-16 15:29:02 +0900

    create project

---
+++ LayoutSample/app/src/main/java/com/thk/layoutsample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.thk.layoutsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
