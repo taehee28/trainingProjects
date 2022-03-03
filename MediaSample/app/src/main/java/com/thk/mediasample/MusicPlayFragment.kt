ed5dfefa3529ec83f71e62d16f6e8e8d53bef044
Kang Taehee <dev.thk28@gmail.com>
2022-03-03 16:58:12 +0900

    음악 seekBar 구현

--- MediaSample/app/src/main/java/com/thk/mediasample/MusicPlayFragment.kt
+++ MediaSample/app/src/main/java/com/thk/mediasample/MusicPlayFragment.kt
@@ -2,11 +2,13 @@ package com.thk.mediasample
 
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.SystemClock
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -60,20 +62,56 @@ class MusicPlayFragment : Fragment() {
                     if (mediaPlayer == null) {
                         mediaPlayer = MediaPlayer().apply {
                             setDataSource(requireContext().resources.openRawResourceFd(R.raw.vivaldi_the_four_seasons))
-                            setOnPreparedListener { btnStateModel.changeBtnState(READY) }
+                            setOnPreparedListener {
+                                btnStateModel.changeBtnState(READY)
+                                binding.seekBar.max = it.duration
+                            }
+                            setOnCompletionListener {
+                                btnStateModel.changeBtnState(STOPPED)
+                            }
                         }
                     }
                     mediaPlayer?.prepare()
                 }
-                PLAYING -> mediaPlayer?.start()
+                PLAYING -> playMusic()
                 PAUSED -> mediaPlayer?.pause()
                 STOPPED -> {
                     mediaPlayer?.stop()
+                    binding.seekBar.progress = 0
                     btnStateModel.changeBtnState(LOADING)
                 }
             }
         })
 
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    mediaPlayer?.seekTo(progress)
+                }
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) { }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) { }
+        })
+    }
+
+    private fun playMusic() {
+        mediaPlayer?.start()
+
+        // SeekBar 움직이기 위한 스레드
+        object : Thread() {
+            override fun run() {
+                super.run()
+
+                while (mediaPlayer?.isPlaying!!) {
+                    activity?.runOnUiThread {
+                        binding.seekBar.progress = mediaPlayer?.currentPosition!!
+                    }
+                    SystemClock.sleep(200)
+                }
+            }
+        }.start()
     }
 
     override fun onStop() {
