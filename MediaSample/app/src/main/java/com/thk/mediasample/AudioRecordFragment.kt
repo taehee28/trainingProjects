ba367b77d748af834ec7e7fb4c2ca5fa64a89ded
Kang Taehee <dev.thk28@gmail.com>
2022-03-03 17:58:30 +0900

    WIP: 퍼미션 승인 구현중

--- MediaSample/app/src/main/java/com/thk/mediasample/AudioRecordFragment.kt
+++ MediaSample/app/src/main/java/com/thk/mediasample/AudioRecordFragment.kt
@@ -1,16 +1,27 @@
 package com.thk.mediasample
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.thk.mediasample.databinding.FragmentAudioRecordBinding
 
 class AudioRecordFragment : Fragment() {
 
     private lateinit var binding: FragmentAudioRecordBinding
-    
+
+    private val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) {
+            
+        } else {
+
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,4 +31,10 @@ class AudioRecordFragment : Fragment() {
         binding = FragmentAudioRecordBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
 }
