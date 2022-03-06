79812d2ea13ee718f0a2ce7d3ae7b753144a7062
taehee28 <dev.thk28@gmail.com>
2022-03-06 22:51:46 +0900

    갤러리 호출 구현

--- MediaSample/app/src/main/java/com/thk/mediasample/ImageControlFragment.kt
+++ MediaSample/app/src/main/java/com/thk/mediasample/ImageControlFragment.kt
@@ -1,17 +1,26 @@
 package com.thk.mediasample
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.thk.mediasample.databinding.FragmentImageControlBinding
 
 class ImageControlFragment : Fragment() {
+    private val TAG = ImageControlFragment::class.simpleName
 
     private var _binding: FragmentImageControlBinding? = null
     private val binding
         get() = _binding!!
+
+    val galleryLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        Log.d(TAG, "result: ${result.data?.data}")
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +29,20 @@ class ImageControlFragment : Fragment() {
     ): View? {
         _binding = FragmentImageControlBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLoadImg.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_PICK).apply {
+                type = MediaStore.Images.Media.CONTENT_TYPE
+                type = "image/*"
+            }
+            galleryLauncher.launch(intent)
+
+        }
     }
 
     override fun onDestroyView() {
