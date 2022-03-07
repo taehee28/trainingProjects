f4fcd8f89d9d41bc77e9daa02b950ff8501a3ba4
Kang Taehee <dev.thk28@gmail.com>
2022-03-07 16:16:25 +0900

    update: 사용 방법 및 코드 변경
    * 선택한 사진을 먼저 보여주지 않고 File로 가지고 있다가 압축하기 버튼을 누르면 보여주기로 변경

--- MediaSample/app/src/main/java/com/thk/mediasample/ImageControlFragment.kt
+++ MediaSample/app/src/main/java/com/thk/mediasample/ImageControlFragment.kt
@@ -6,19 +6,13 @@ import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.net.toUri
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
-import com.bumptech.glide.request.transition.Transition
+import androidx.navigation.fragment.findNavController
 import com.thk.mediasample.databinding.FragmentImageControlBinding
 import com.thk.mediasample.util.GlideApp
 import kotlinx.coroutines.*
@@ -32,30 +26,40 @@ class ImageControlFragment : Fragment() {
     private val binding
         get() = _binding!!
 
-    private val galleryLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        loadCurrentPhotoInto(binding.imageView, result.data?.data)
+    private var photoFile: File? = null
+
+    private val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { result ->
+        if (result.any { permission -> !permission.value }) {
+            AlertDialog.Builder(requireContext())
+                .setMessage("[설정] > [개인정보 보호] > [권한 관리자]에서 권한을 허락해주세요.")
+                .setPositiveButton("확인") { dialoginterface, n ->
+                    findNavController().navigateUp()
+                    dialoginterface.dismiss()
+                }
+                .setCancelable(false)
+                .show()
+        }
     }
 
-    private fun loadCurrentPhotoInto(view: ImageView, uri: Uri?) {
+    private val galleryLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        loadPickedPhotoAsFile(result.data?.data)
+    }
+
+    private fun loadPickedPhotoAsFile(uri: Uri?) {
         uri?.let {
             CoroutineScope(Dispatchers.Default).launch {
-                val pickedBitmap = GlideApp.with(view)
-                    .asBitmap()
-                    .override(Target.SIZE_ORIGINAL)
-                    .load(it)
-                    .submit().get()
-
-                val outputStream = ByteArrayOutputStream()
-                pickedBitmap.compress(Bitmap.CompressFormat.JPEG, 10, outputStream)
-                val compressedBitmap = BitmapFactory.decodeByteArray(outputStream.toByteArray(), 0, outputStream.size())
-
-                pickedBitmap.recycle()
+                photoFile = GlideApp.with(requireContext()).asFile().load(it).submit().get()
 
                 withContext(Dispatchers.Main) {
-                    view.setImageBitmap(compressedBitmap)
+                    displayFilePath(photoFile?.absolutePath)
                 }
+
             }
         }
+    }
+
+    private fun displayFilePath(path: String?) {
+        binding.tvFilePath.text = path
     }
 
     override fun onCreateView(
@@ -70,12 +74,40 @@ class ImageControlFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        permissionLauncher.launch(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE))
+
+        displayFilePath(photoFile?.absolutePath)
+
         binding.btnLoadImg.setOnClickListener {
             val intent = Intent(Intent.ACTION_PICK).apply {
                 type = MediaStore.Images.Media.CONTENT_TYPE
                 type = "image/*"
             }
             galleryLauncher.launch(intent)
+        }
+
+        binding.btnCompress.setOnClickListener {
+            if (photoFile == null) return@setOnClickListener
+
+            CoroutineScope(Dispatchers.Default).launch {
+                val outputStream = ByteArrayOutputStream()
+
+                GlideApp.with(requireContext())
+                    .asBitmap()
+                    .load(photoFile)
+                    .submit()
+                    .get().run {
+                    compress(Bitmap.CompressFormat.JPEG, 10, outputStream)
+                    recycle()
+                }
+
+                val compressedBitmap = BitmapFactory.decodeByteArray(outputStream.toByteArray(), 0, outputStream.size())
+
+                withContext(Dispatchers.Main) {
+                    GlideApp.with(requireContext()).load(compressedBitmap).into(binding.imageView)
+                }
+                
+            }
         }
     }
 
