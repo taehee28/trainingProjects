f28843b1c5f85e4fc0385eeef9ca3ca93640b61f
Kang Taehee <dev.thk28@gmail.com>
2022-03-08 12:25:24 +0900

    coroutine scope 수정

--- MediaSample/app/src/main/java/com/thk/mediasample/ImageControlFragment.kt
+++ MediaSample/app/src/main/java/com/thk/mediasample/ImageControlFragment.kt
@@ -14,6 +14,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.thk.mediasample.databinding.FragmentImageControlBinding
 import com.thk.mediasample.util.GlideApp
@@ -74,7 +75,7 @@ class ImageControlFragment : Fragment() {
         binding.btnCompress.setOnClickListener {
             if (photoFile == null) return@setOnClickListener
 
-            CoroutineScope(Dispatchers.Default).launch {
+            lifecycleScope.launch {
 
                 val compressedBitmap = compressBitmap()
 
@@ -96,7 +97,7 @@ class ImageControlFragment : Fragment() {
 
     private fun loadPickedPhotoAsFile(uri: Uri?) {
         uri?.let {
-            CoroutineScope(Dispatchers.Default).launch {
+            lifecycleScope.launch {
                 photoFile = GlideApp.with(requireContext()).asFile().load(it).submit().get()
 
                 withContext(Dispatchers.Main) {
