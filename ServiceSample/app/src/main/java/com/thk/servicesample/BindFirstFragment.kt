09f212f327545295e5d3cf56045ae94654b66745
Kang Taehee <dev.thk28@gmail.com>
2022-03-11 09:59:49 +0900

    create: BindService 화면 및 프래그먼트 기초 구현

---
+++ ServiceSample/app/src/main/java/com/thk/servicesample/BindFirstFragment.kt
@@ -0,0 +1,23 @@
+package com.thk.servicesample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.thk.servicesample.databinding.FragmentBindFirstBinding
+
+class BindFirstFragment : BaseFragment<FragmentBindFirstBinding>() {
+
+
+    override fun getBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentBindFirstBinding {
+        return FragmentBindFirstBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
