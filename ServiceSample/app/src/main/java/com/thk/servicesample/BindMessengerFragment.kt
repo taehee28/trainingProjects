ba87d58313b15c2d7d76e0e4d99ff9b6c75b0934
Kang Taehee <dev.thk28@gmail.com>
2022-03-16 13:56:34 +0900

    feat: 양방향 메신저 구현

--- ServiceSample/app/src/main/java/com/thk/servicesample/BindMessengerFragment.kt
+++ ServiceSample/app/src/main/java/com/thk/servicesample/BindMessengerFragment.kt
@@ -14,22 +14,41 @@ import com.thk.servicesample.service.MessengerService
 
 class BindMessengerFragment : BaseFragment<FragmentBindMessengerBinding>() {
 
-    private var messenger: Messenger? = null
+    // 서비스에서 오는 메세지를 처리할 핸들러
+    private val clientHandler = object : Handler(Looper.myLooper()!!) {
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                MessengerService.MSG_SAY_HELLO -> Toast.makeText(requireContext(), "Hello from Client", Toast.LENGTH_SHORT).show()
+                else -> super.handleMessage(msg)
+            }
+        }
+    }
+    // 서비스의 메신저
+    private var serviceMessenger: Messenger? = null
+    // 서비스한테 보낼 클라이언트의 메신저
+    private val clientMessenger = Messenger(clientHandler)
     private var isBound = false
 
     private val connection = object : ServiceConnection {
         override fun onServiceConnected(p0: ComponentName?, binder: IBinder?) {
-            messenger = Messenger(binder)
+            // 서비스가 onBind에서 반환하는 IBinder를 받아 메신저의 인스턴스 생성
+            serviceMessenger = Messenger(binder).apply {
+                // 서비스에 바운딩되면 클라이언트의 메신저를 서비스한테 메세지로 보냄
+                send(
+                    Message.obtain(null, MessengerService.MSG_BIND_CLIENT, 0, 0).apply {
+                        // 서비스에게 보낼 클라이언트 메신저 담기
+                        replyTo = clientMessenger
+                    }
+                )
+            }
             isBound = true
 
             Toast.makeText(requireContext(), "Service is bound", Toast.LENGTH_SHORT).show()
         }
 
         override fun onServiceDisconnected(p0: ComponentName?) {
-            messenger = null
+            serviceMessenger = null
             isBound = false
-
-            Toast.makeText(requireContext(), "Service is unbound", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -51,30 +70,58 @@ class BindMessengerFragment : BaseFragment<FragmentBindMessengerBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnSendMessage.setOnClickListener {
-            sendHello()
-        }
+        binding.btnSendMessage.setOnClickListener { sendHello() }
+        binding.btnReceiveMessage.setOnClickListener { receiveHello() }
     }
 
+    /**
+     * 서비스에게 토스트 띄우도록 메세지 보내기
+     */
     private fun sendHello() {
         if (!isBound) return
 
-        val msg = Message.obtain(null, MessengerService.MSG_SAY_HELLO, 0, 0)
+        val msg = Message.obtain(null, MessengerService.MSG_SAY_HELLO)
 
-        try {
-            messenger?.send(msg)
-        } catch (e: RemoteException) {
-            e.printStackTrace()
-            Toast.makeText(requireContext(), "failed", Toast.LENGTH_SHORT).show()
+        trySend {
+            serviceMessenger?.send(msg)
+        }
+    }
+
+    /**
+     * 서비스에게 클라이언트로 토스트 띄우는 메세지를 보내라고 메세지 보내기
+     */
+    private fun receiveHello() {
+        if (!isBound) return
+
+        val msg = Message.obtain(null, MessengerService.MSG_SEND_TO_CLIENT)
+
+        trySend {
+            serviceMessenger?.send(msg)
         }
     }
 
     override fun onStop() {
+        // 서비스 바운드 해제
         if (isBound) {
+            // 서비스가 가지는 클라이언트의 메신저 해제처리 요청 보내기
+            trySend {
+                serviceMessenger?.send(
+                    Message.obtain(null, MessengerService.MSG_UNBIND_CLIENT)
+                )
+            }
             requireActivity().unbindService(connection)
             isBound = false
         }
 
         super.onStop()
+    }
+
+    private inline fun trySend(block: () -> Unit) {
+        try {
+            block()
+        } catch (e: RemoteException) {
+            e.printStackTrace()
+            Toast.makeText(requireContext(), "send failed", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- ServiceSample/app/src/main/java/com/thk/servicesample/service/MessengerService.kt
+++ ServiceSample/app/src/main/java/com/thk/servicesample/service/MessengerService.kt
@@ -5,14 +5,19 @@ import android.content.Context
 import android.content.Intent
 import android.os.*
 import android.widget.Toast
+import com.thk.servicesample.util.logd
 
 class MessengerService : Service() {
 
     companion object {
         const val MSG_SAY_HELLO = 1
+        const val MSG_SEND_TO_CLIENT = 2
+        const val MSG_BIND_CLIENT = 3
+        const val MSG_UNBIND_CLIENT = 4
     }
 
-    private lateinit var messenger: Messenger
+    // 서비스의 메신저 
+    private lateinit var serviceMessenger: Messenger
 
     /**
      * 메세지를 처리할 핸들러 구현
@@ -21,16 +26,46 @@ class MessengerService : Service() {
         context: Context,
         private val applicationContext: Context = context.applicationContext
     ): Handler(Looper.myLooper()!!) {
+
+        // 클라이언트의 메신저
+        private var clientMessenger: Messenger? = null
+
         override fun handleMessage(msg: Message) {
             when (msg.what) {
-                MSG_SAY_HELLO -> Toast.makeText(applicationContext, "Hello!", Toast.LENGTH_SHORT).show()
+                // 클라이언트가 보내준 클라이언트의 메신저 획득
+                MSG_BIND_CLIENT -> {
+                    clientMessenger = msg.replyTo
+                    logd("connect")
+                }
+
+                // 서비스 unbind 할 때 메신저도 null 처리
+                MSG_UNBIND_CLIENT -> {
+                    clientMessenger = null
+                    logd("disconnect")
+                }
+
+                // 클라이언트에게 메신저로 메세지 보내기
+                MSG_SEND_TO_CLIENT -> {
+                    val msg = Message.obtain(null, MSG_SAY_HELLO)
+
+                    try {
+                        clientMessenger?.send(msg)
+                    } catch (e: RemoteException) {
+                        e.printStackTrace()
+                    }
+                }
+
+                // 토스트 띄우기
+                MSG_SAY_HELLO -> Toast.makeText(applicationContext, "Hello from Service", Toast.LENGTH_SHORT).show()
                 else -> super.handleMessage(msg)
             }
         }
     }
 
     override fun onBind(intent: Intent?): IBinder? {
-        messenger = Messenger(IncomingHandler(this))
-        return messenger.binder
+        // 메신저 인스턴스 생성하기
+        serviceMessenger = Messenger(IncomingHandler(this))
+        // 메신저가 생성하는 IBinder 리턴
+        return serviceMessenger.binder
     }
 }
