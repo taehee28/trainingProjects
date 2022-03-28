91ec8818088476792ae22bfebc6b874aa0b4fa92
Kang Taehee <dev.thk28@gmail.com>
2022-03-28 15:48:12 +0900

    feat: 포스터 collapsingToolbarLayout 구현
    * todo: 로딩 늦게될 때 접혀있는거 해결해야 함

--- MovieRanking/app/src/main/java/com/thk/movieranking/MovieDetailFragment.kt
+++ MovieRanking/app/src/main/java/com/thk/movieranking/MovieDetailFragment.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import com.thk.movieranking.databinding.FragmentMovieDetailBinding
+import com.thk.movieranking.network.MovieApiService
+import com.thk.movieranking.utils.GlideApp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MovieDetailFragment : BaseFragment<FragmentMovieDetailBinding>() {
 
-    val args: MovieDetailFragmentArgs by navArgs()
+    private val args: MovieDetailFragmentArgs by navArgs()
 
     override fun getBinding(
         inflater: LayoutInflater,
@@ -22,6 +28,16 @@ class MovieDetailFragment : BaseFragment<FragmentMovieDetailBinding>() {
         super.onViewCreated(view, savedInstanceState)
 
         val movieId = args.movieId
-        logd(movieId.toString())
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            val movieDetail = MovieApiService.api.getMovieDetail(movieId)
+            logd(movieDetail.toString())
+
+            withContext(Dispatchers.Main) {
+                GlideApp.with(binding.ivPoster)
+                    .load(TMDB_IMAGE_URL + movieDetail.poster_path)
+                    .into(binding.ivPoster)
+            }
+        }
     }
 }
--- MovieRanking/app/src/main/java/com/thk/movieranking/network/MovieApiInterface.kt
+++ MovieRanking/app/src/main/java/com/thk/movieranking/network/MovieApiInterface.kt
@@ -1,10 +1,12 @@
 package com.thk.movieranking.network
 
 import com.thk.movieranking.BuildConfig
+import com.thk.movieranking.models.Movie
 import com.thk.movieranking.models.MovieListResponse
 import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MovieApiInterface {
@@ -23,4 +25,11 @@ interface MovieApiInterface {
         @Query("language") language: String = "ko-KR",
         @Query("region") region: String = "KR"
     ): MovieListResponse
+
+    @GET("movie/{movie_id}")
+    suspend fun getMovieDetail(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY,
+        @Query("language") language: String = "ko-KR"
+    ) : Movie
 }
